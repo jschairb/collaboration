03dde181b416fbbdd93c74efbef51e083d696261
Josh Schairbaum <joshua.schairbaum@gmail.com>
2008-09-07 21:47:32 -0400

    adding flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def flash_div *keys
+    divs = keys.select { |k| flash[k] }.collect do |k|
+      content_tag :div, flash[k], :class => "flash #{k}" 
+    end
+    divs.join
+  end
+
 end
